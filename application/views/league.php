a72e417c9d576d30f6374133df052a2bad5bdf79
Duy Le <duyt.le@hotmail.ca>
2015-10-01 16:07:02 -0700

    Completed About page content

---
+++ application/views/league.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>League</title>
+</head>
+
+<body>
+
+<div id="container">
+	<h1>League Page</h1>
+</div>
+
+</body>
+</html>
---
+++ application/views/team.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Team</title>
+</head>
+
+<body>
+
+<div id="container">
+	<h1>Team Page</h1>
+</div>
+
+</body>
+</html>
