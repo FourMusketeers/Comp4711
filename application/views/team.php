96143316ab8305a57007874240c851b36072f715
Duy Le <duyt.le@hotmail.ca>
2015-10-01 18:36:16 -0700

    Completed Team page

--- application/views/team.php
+++ application/views/team.php
@@ -10,27 +10,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
 <script src="semantic/dist/semantic.min.js"></script>
 
-<div style="height:100%; width:100%; padding:50px;">
-	<h1 style="color:#006699;">TEAM</h1>
-	
+<div>
+	<h1>TEAM</h1>
+
 	<table class="ui single line table">
-  <thead>
-    <tr>
-      <th>Bill Bob</th>
-	  <th>Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Jill Lewis</td>
-      <td>jilsewris22@yahoo.com</td>
-    </tr>
-    <tr>
-      <td>John Lilki</td>
-      <td>September 14, 2013</td>
-    </tr>
-  </tbody>
-</table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Wins</th>
+				<th>Losses</th>
+			</tr>
+		</thead>
+		<tbody>
+			{teams}
+			<tr>
+				<td>{name}</td>
+				<td>{wins}</td>
+				<td>{losses}</td>
+			</tr>
+			{/teams}
+			<?php //echo $first_name; ?>
+			
+		</tbody>
+	</table>
 </div>
 
 </body>
