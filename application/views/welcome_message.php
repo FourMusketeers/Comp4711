d63285eb99200d4101a3ebbb563f83ad790c31d4
LeonHo <leon.ho@bchydro.com>
2015-10-01 19:31:22 -0700

    Fixed about page, and added image to welcome page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,3 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+<div id="appMasthead" class="ui inverted vertical center aligned segment">
+    <div class="ui text container appCenterVertical">
+      <h1 class="ui inverted header">
+        Roster Manager
+      </h1>
+      <h2>Start Managing Your Roster Now!</h2>
+      <a href="/team">
+      	<div class="ui huge primary button">View Team<i class="right arrow icon"></i></div>
+      </a>
+    </div>
+
+  </div>
